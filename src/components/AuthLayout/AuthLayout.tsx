ff3a8c1d89420d4de5646541fb3eb3da8725662e
asmaa <hossamasmaa07@gmail.com>
2025-10-04 03:52:44 +0300

    register

--- src/components/AuthLayout/AuthLayout.tsx
+++ src/components/AuthLayout/AuthLayout.tsx
@@ -7,16 +7,17 @@ export default function AuthLayout() {
   return (
     <>
       <div className="min-h-screen bg-black flex flex-col md:flex-row">
-        {/* Left Section */}
-        <div className="flex flex-col p-8 md:w-1/2">
-          <img className="w-40 md:w-52 mb-9 mx-auto md:mx-0" src={logo} alt="auth logo" />
-          <div className="flex justify-center md:justify-start items-center">
-            <Outlet />
-          </div>
-        </div>
+       {/* Left Section */}
+<div className="flex flex-col p-8 md:w-1/2">
+  <img className="w-40 md:w-52 mb-9 mx-auto md:mx-0" src={logo} alt="auth logo" />
+  <div className="flex w-full justify-center md:justify-start items-center md:items-start">
+    <Outlet />
+  </div>
+</div>
+
 
         {/* Right Section */}
-        <div className="md:w-1/2 flex items-center justify-center p-5">
+        <div className="md:w-1/2 flex items-center justify-center p-8">
           <img className="w-full h-auto object-cover rounded-lg" src={authimg} alt="auth img" />
         </div>
       </div>
